b69a7c0961f3dc67c2383044d0d0aaaefc3b1c6c
Jon Mullingtons <jmulhern85@gmail.com>
2024-07-13 18:17:13 +0100

    Initial HTML, no css or functionality yet

--- lib/phoenixdate_web/router.ex
+++ lib/phoenixdate_web/router.ex
@@ -18,6 +18,7 @@ defmodule PhoenixdateWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live "/datepicker", DatePickerLive
   end
 
   # Other scopes may use custom stacks.
